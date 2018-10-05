7a9842a96797e30eb34c302bf68f9c2171d38046
Lise Brackbill <menagerie@hevanet.com>
2018-10-04 19:24:19 -0700

    more confusion

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
-var count = 1;
-function sayHello () {
-    console.log('hello world!, for the ', count, 'time');
-    count++;
+function handleSubmit(event) {
+    var form = event.target;
+    var elements = form.elements;
+    var name = elements.name.value;
+    console.log('name', name);
+
+    var comments = elements.comments.value;
+    console.log('comments', comments);
+
+    var toppings = elements.toppings.value;
+    console.log('toppings', toppings);
 }
